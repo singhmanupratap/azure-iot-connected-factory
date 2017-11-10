5d59b9054c5fbcf4aeae34316c803c6b1b345f6f
Manu Pratap Singh <manu.a.pratap.singh@accenture.com>
2017-11-10 10:46:19 +0100

    cmmit

--- UpdateBrowserEndpoints.ps1
+++ UpdateBrowserEndpoints.ps1
@@ -1,6 +1,6 @@
 ﻿# Use a copy of the original to patch
-$script:IoTSuiteRootPath = "$artifactStagingDirectory"
-$script:WebAppLocalPath = "$artifactStagingDirectory"
+$script:IoTSuiteRootPath = "$env:artifactStagingDirectory"
+$script:WebAppLocalPath = "$env:artifactStagingDirectory"
 #$script:IoTSuiteRootPath = "C:\Users\manu.a.pratap.singh\Source\Repos\azure-iot-connected-factory"
 $script:WebAppPath = "$script:IoTSuiteRootPath\WebApp"
 $script:TopologyDescription = "$script:WebAppPath/Contoso/Topology/ContosoTopologyDescription.json"
