17ab87e283a9810d637fce3653db3b329d3f3686
Manu Pratap Singh <manu.a.pratap.singh@accenture.com>
2017-11-13 15:11:50 +0100

    Parameter added for build id

--- PostBuildInformation.ps1
+++ PostBuildInformation.ps1
@@ -1,11 +1,13 @@
-$buildId=$Env:BuildId
-$VmArmTemplateUri=$env:vmDeploymentTemplateFile+"/"+"$buildId"+"/"+"FactorySimulation.json"+$env:sasToken
-$SimulationUri=$env:simulationUri+"/"+"$buildId"+"/"+"FactorySimulation.json"+$env:sasSimulationUriToken
-$InitSimulationUri=$env:initSimulationUri+"/"+"$buildId"+"/"+"FactorySimulation.json"+$env:sasInitSimulationUriToken
-$WebAppLocalPath=$env:webAppLocalPath+"/"+"$buildId"+"/"+"FactorySimulation.json"+$env:WebAppLocalPath
-$uri="http://api-ondemanddemo.azurewebsites.net/api/builds/complete?buildId="+$buildId;
+Param(
+	[string]$BuildId
+)
+$VmArmTemplateUri=$env:vmDeploymentTemplateFile+"/"+"$BuildId"+"/"+"FactorySimulation.json"+$env:sasToken
+$SimulationUri=$env:simulationUri+"/"+"$BuildId"+"/"+"FactorySimulation.json"+$env:sasSimulationUriToken
+$InitSimulationUri=$env:initSimulationUri+"/"+"$BuildId"+"/"+"FactorySimulation.json"+$env:sasInitSimulationUriToken
+$WebAppLocalPath=$env:webAppLocalPath+"/"+"$BuildId"+"/"+"FactorySimulation.json"+$env:WebAppLocalPath
+$uri="http://api-ondemanddemo.azurewebsites.net/api/builds/complete?buildId="+$BuildId;
 $pkgUrl=$VmArmTemplateUri+"|"+$SimulationUri+"|"+$InitSimulationUri+"|"+$WebAppLocalPath
-$qry=@{"Status"=2;"Description"="Build Completed successfully";"VSTSBuildId"=$buildId;"PkgURL"=$pkgUrl};
+$qry=@{"Status"=2;"Description"="Build Completed successfully";"VSTSBuildId"=$BuildId;"PkgURL"=$pkgUrl};
 $json=$qry|ConvertTo-Json;
 Write-Output $json
 $response=Invoke-RestMethod -Method PUT -Uri $uri -ContentType "application/json" -Headers @{} -Body $json;
